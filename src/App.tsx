eae58aae99f2ca5f4f1dcc08a075623892f8febe
Emichu <k.kreplewicz@gmail.com>
2020-04-20 17:38:29 +0100

    added homepage and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,14 @@
 import React from 'react';
-
-//api call
-async function getList() {
-  const listUrl = "https://jsonplaceholder.typicode.com/posts";
-  const queryOptions = {};
-
-  const response = await fetch(listUrl, queryOptions);
-
-  return response.json();
-}
+import routes from './routes';
+import { BrowserRouter } from "react-router-dom";
+import { renderRoutes } from "react-router-config";
 
 function App() {
-  const [data, setData] = React.useState([])
-  const [searchTerm, setSearchTerm] = React.useState("");
-
-  React.useEffect(() => {
-    getList().then((data) => {
-      setData(data)
-    })
-  }, [])
   return (
-    <div>
-      <input
-        onChange={event => setSearchTerm(event.target.value)}
-      />
-
-      <ul>
-        {data
-          .filter((data: any) => data.title.includes(searchTerm))
-          .map((item: any, index: number) => <li key={index} >{item.title}</li>)}
-      </ul>
-    </div>
-  );
+    <BrowserRouter>
+      {renderRoutes(routes)}
+    </BrowserRouter>
+  )
 
 }
 
---
+++ src/pages/Home.page.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+async function getList() {
+  const listUrl = "https://jsonplaceholder.typicode.com/posts";
+  const queryOptions = {};
+
+  const response = await fetch(listUrl, queryOptions);
+
+  return response.json();
+}
+
+function HomePage() {
+  const [data, setData] = React.useState([])
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  React.useEffect(() => {
+    getList().then((data) => {
+      setData(data)
+    })
+  }, [])
+  return (
+    <div>
+      <input
+        onChange={event => setSearchTerm(event.target.value)}
+      />
+
+      <ul>
+        {data
+          .filter((data: any) => data.title.includes(searchTerm))
+          .map((item: any, index: number) => <li key={index} >{item.title}</li>)}
+      </ul>
+    </div>
+  );
+};
+
+export default HomePage;
